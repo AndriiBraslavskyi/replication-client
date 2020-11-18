fa67f2debadf554b30767eab079855e9f596d9ed
BraslavskyiAndrey <braslavskyiandrey@gmail.com>
2020-11-18 23:10:30 +0200

    Log message with configs during startup

--- src/main/java/com/de/configurations/WebConfiguration.java
+++ src/main/java/com/de/configurations/WebConfiguration.java
@@ -4,6 +4,8 @@ import com.de.repositories.InMemoryMessageRepository;
 import com.de.repositories.MessageRepository;
 import com.de.services.MessageService;
 import com.de.services.MessageServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +13,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WebConfiguration {
 
+    final private static Logger logger = LoggerFactory.getLogger(WebConfiguration.class);
+
     @Bean
     MessageService messageService(MessageRepository messageRepository,
                                   @Value("${replication-client.delay}") Integer delay) {
+        logger.info("Client started with delay = {}", delay);
         return new MessageServiceImpl(messageRepository, delay);
     }
 
